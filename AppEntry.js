cc9fa90b9f32f1d0584dd4eb609513479be88117
Bosancu Robert <r_bosancu80@yahoo.co.uk>
2022-05-18 19:01:57 +0300

    modified json

--- AppEntry.js
+++ AppEntry.js
@@ -5,6 +5,7 @@ import { personalizare } from './theme/mainTheme';
 import MorcovulPrincipal from './components/MorcovulPrincipal';
 import TextSalut from './components/TextSalut';
 import { culori } from './theme/nuante';
+import {alimente} from './logica/alimente';
 
 export default function App() {
   return (
@@ -15,7 +16,7 @@ export default function App() {
       
       <View style={{flex:1}}>
 
-        <TextSalut stil={personalizare.textPrezentareRosu} mesaj="Eu sunt un mesaj rosu"/>
+        <TextSalut stil={personalizare.textPrezentareRosu} mesaj={Object.keys(alimente.mic_dejun).length}/>
         <TextSalut stil={personalizare.textPrezentareRoz} mesaj="Eu sunt roz"/>
         <TextSalut stil={personalizare.textPrezentareAlbastru} mesaj="Eu sunt baietel"/>
       </View>
